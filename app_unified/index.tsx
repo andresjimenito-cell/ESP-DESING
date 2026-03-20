5c6c31fedcf4276b2fc278a757f718dac01af95e
andresjimenito-cell <andresjimenito@gmail.com>
2026-03-20 09:10:44 -0500

    fix: import index.css in entry point so Vite bundles all styles

--- app_unified/index.tsx
+++ app_unified/index.tsx
@@ -1,9 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import { LanguageProvider } from './i18n';
 import { ThemeProvider } from './theme';
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
